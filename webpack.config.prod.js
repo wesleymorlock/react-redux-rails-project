045ba5a6ea49bdccfcf6dfa1e2a18eac496d314f
Wesley Morlock <wjm024@bucknell.edu>
2018-08-31 15:23:42 -0700

    Remove sqlite and add prod webpack config file

---
+++ webpack.config.prod.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+var path = require('path');
+module.exports = {
+  context: __dirname,
+  entry: ['./app/frontend/javascripts/main.js'],
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js',
+    publicPath: '/assets'
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
