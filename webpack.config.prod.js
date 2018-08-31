cb222c227e0bd86db7074f1d7d31cf105e34d9cc
Wesley Morlock <wjm024@bucknell.edu>
2018-08-31 16:07:13 -0700

    Remove port from process.env on webpack config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -15,8 +15,7 @@ module.exports = {
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production'),
-        'PORT': 3000
+        'NODE_ENV': JSON.stringify('production')
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
